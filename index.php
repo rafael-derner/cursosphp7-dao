bb29549551ae738ba9fcfe50f49a04fb9e630efe
Rafael Derner de Oliveira <adm.derner@ebserh.gov.br>
2020-10-12 15:37:25 -0300

    update classe usuario

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 
 require_once("config.php");
 
+/*$usuario = new Usuario();
+$usuario->setDeslogin("rafael");
+$usuario->setDessenha("abc123");
+*/
+
+/*carrega um usuario
 $root = new Usuario();
-
 $root->loadById(3);
-
 echo $root;
+*/
+
+/*carrega uma lista de usuarios
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/*carrega uma lista de usuarios buscando pelo login
+$search = Usuario::search("r");
+echo json_encode($search);
+*/
+
+/*carrega usuario usando login e sehanha*/
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+
+echo $usuario;
 
 ?>
